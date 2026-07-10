9c9aa4d091e83a4943d38ccb0cbb6e0cd8ad94bf
jvazquez30 <josevazquez881@gmail.com>
2026-07-09 17:28:31 -0700

    connected Timer Controls/Display in Pomodoro

--- src/app/components/PomodoroTimer.tsx
+++ src/app/components/PomodoroTimer.tsx
@@ -1,22 +1,53 @@
 "use client"
 import { useState, useEffect, useRef } from "react"
+import TimerDisplay from "./TimerDisplay";
+import TimerControls from "./TimerControls";
 
 
 export default function PomodoroTimer() {
+  const defaultNum = 25
   const [isRunning, setIsRunning] = useState(false)
-  const [secondsLeft, setSecondsLeft] = useState(25 * 60);
+  const [secondsLeft, setSecondsLeft] = useState(defaultNum * 60);
   const endTimeRef = useRef<number | null>(null);
 
-useEffect(() => {
-  if (!isRunning) return;
-  endTimeRef.current = Date.now() + secondsLeft * 1000
+  useEffect(() => {
+    if (!isRunning) return;
+    endTimeRef.current = Date.now() + secondsLeft * 1000 // gives a fixed timer for endTimeRef
 
-  const id = setInterval(() => {
-    if (endTimeRef.current === null) return;
-  setSecondsLeft(Math.round((endTimeRef.current - Date.now())/ 1000))
-  }, 1000)
+    const id = setInterval(() => {
+      if (endTimeRef.current === null) return; // in case endTime is null
+      setSecondsLeft(Math.round((endTimeRef.current - Date.now()) / 1000))
+    }, 1000)
 
- return () => clearInterval(id)
-},[isRunning])
+    return () => clearInterval(id)
+  }, [isRunning])
 
+  const handlePause = () => {
+    setIsRunning(false);
+  };
+
+  const handleStart = () => {
+    setIsRunning(true)
+  }
+
+  const handleReset = () => {
+    setIsRunning(false)
+    setSecondsLeft(defaultNum * 60)
+  }
+
+
+
+  return (
+    <div>
+     <TimerDisplay secondsLeft={secondsLeft}/>
+
+     <TimerControls 
+     isRunning={isRunning}
+     onPause={handlePause}
+     onStart={handleStart}
+     onReset={handleReset}
+     />
+      
+    </div>
+  )
 }
