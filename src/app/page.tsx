4ad7b92f2778fdfa37d46f1fdd18e99514036efa
jvazquez30 <josevazquez881@gmail.com>
2026-07-13 12:01:10 -0700

    connected PomodorTimer to main page fiel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import PomodoroTimer from "./components/PomodoroTimer";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-blue-500
        sm:items-start">
         This page will display the timer (like this)
+        <PomodoroTimer />
       </main>
     </div>
   );
